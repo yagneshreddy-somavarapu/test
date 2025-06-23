52badd0ea42fe5587fd9b2f70ba6d235e4eb7f01
yagneshreddy-somavarapu <yagneshreddysomavarapu@gmail.com>
2025-06-23 12:51:38 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div>
+      <h1>welcome</h1>
+      <h2>mmy firs project</h2>
+     </div>
+    </>
+  )
+}
+
+export default App
